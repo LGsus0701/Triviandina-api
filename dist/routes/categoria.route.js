27c9e31e403272e03630c3fabd8706374ee43cc2
LGsus0701 <yisus.qapac@gmail.com>
2026-01-08 00:19:41 -0500

    api final

--- dist/routes/categoria.route.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const categoria_controller_1 = require("../controllers/categoria.controller");
-const router = (0, express_1.Router)();
-//GET / --- > listarCategoria
-router.get('/listar', categoria_controller_1.listarCategoria);
-exports.default = router;
-//# sourceMappingURL=categoria.route.js.map
--- src/routes/categoria.route.ts
+++
@@ -1,9 +0,0 @@
-import {    Router } from "express";
-import { listarCategoria } from '../controllers/categoria.controller';
-
-const router: Router = Router();
-
-//GET / --- > listarCategoria
-router.get('/listar', listarCategoria);
-
-export default router;
